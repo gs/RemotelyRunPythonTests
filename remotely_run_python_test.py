8457938d6da55ff552481a850a7ad90a63e70820
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2013-08-19 09:40:43 +0200

    try to support sndbox

--- remotely_run_python_test.py
+++ remotely_run_python_test.py
@@ -4,10 +4,11 @@ import re
 
 class RemotelyRunPythonTest(sublime_plugin.WindowCommand):
 	def run(self):
-		path, cmnd = self.settings()
+		path, cmnd, out = self.settings()
 		file_name = self.window.active_view().file_name().replace(str(path), '')
 		if re.search('_test.py', file_name):
 			self.window.run_command('exec', {'cmd': [cmnd + file_name], 'shell':True} )
+			self.window.run_command('exec', {'cmd': [out], 'shell':True} )
 		else:
 			sublime.error_message("Wtf? Is this a test file?!")
 
@@ -15,7 +16,8 @@ class RemotelyRunPythonTest(sublime_plugin.WindowCommand):
 		settings = sublime.load_settings("remotely_run_python_test.sublime-settings")
 		path = settings.get("path")
 		cmd = settings.get("cmd")
+		out = settings.get("out")
 		if (path == "") or (cmd == ""):
 			sublime.error_message("Please define 'cmd' and 'path' in remotely_run_python_test.sublime-settings")
 		else:
-			return [path, cmd]
+			return [path, cmd, out]
